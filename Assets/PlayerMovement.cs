0d0048f3f479e4096d40bf816c09b9fd49468c9c
Miles602 <milesmd1426@gmail.com>
2024-02-08 19:50:05 -0500

    Basic interaction system

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MoveSmoothTime;
+    public float GravityStrength;
+    public float JumpStrength;
+    public float WalkSpeed;
+    public float RunSpeed;
+
+    private CharacterController Controller;
+
+    private Vector3 CurrentMoveVelocity;
+    private Vector3 MoveDampVelocity;
+    private Vector3 CurrentForceVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 PlayerInput = new Vector3
+        {
+            x = Input.GetAxisRaw("Horizontal"),
+            y = 0f,
+            z = Input.GetAxisRaw("Vertical")
+        };
+
+        if (PlayerInput.magnitude > 1f)
+        {
+            PlayerInput.Normalize();
+        }
+
+        Vector3 MoveVector = transform.TransformDirection(PlayerInput);
+
+        float CurrentSpeed = Input.GetKey(KeyCode.LeftShift) ? RunSpeed : WalkSpeed;
+
+        CurrentMoveVelocity = Vector3.SmoothDamp(
+            CurrentMoveVelocity,
+            MoveVector * CurrentSpeed,
+            ref MoveDampVelocity,
+            MoveSmoothTime
+        );
+
+        Controller.Move(CurrentMoveVelocity * Time.deltaTime);
+
+        Ray GroundCheckRay = new Ray(transform.position, Vector3.down);
+
+        if (Physics.Raycast(GroundCheckRay, 1.1f))
+        {
+            CurrentForceVelocity.y = -2f;
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                CurrentForceVelocity.y = JumpStrength;
+            }
+        }
+        else
+        {
+            CurrentForceVelocity.y -= GravityStrength * Time.deltaTime;
+        }
+
+        Controller.Move(CurrentForceVelocity * Time.deltaTime);
+    }
+}
