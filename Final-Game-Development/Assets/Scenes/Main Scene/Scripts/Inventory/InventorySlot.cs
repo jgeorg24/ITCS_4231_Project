1ccf7d15650fe9f78c82a9f749fd610e1ce00576
Derek_W <dwang26@uncc.edu>
2024-05-06 15:09:44 -0400

    merge derek-final to miles branch

--- Final-Game-Development/Assets/Scenes/Main Scene/Scripts/Inventory/InventorySlot.cs
+++ Final-Game-Development/Assets/Scenes/Main Scene/Scripts/Inventory/InventorySlot.cs
@@ -8,9 +8,9 @@ public class InventorySlot : MonoBehaviour, IDropHandler
         if (transform.childCount == 0)
         {
             GameObject dropped = eventData.pointerDrag;
-            EquipItem equipItem = dropped.GetComponent<EquipItem>(); 
-            equipItem.parentAfterDrag = transform;
-            equipItem.equippedSlot = this; // Remember the slot where the item is equipped
+            InventoryItem inventoryItem = dropped.GetComponent<InventoryItem>(); 
+            inventoryItem.parentAfterDrag = transform; // Access parentAfterDrag through an instance of InventoryItem
+            //equipItem.equippedSlot = this; // Remember the slot where the item is equipped
         }
     }
 }
--- Final-Game-Development/Assets/Scenes/Main Scene/Scripts/Inventory/KeyboardInventory.cs
+++ Final-Game-Development/Assets/Scenes/Main Scene/Scripts/Inventory/KeyboardInventory.cs
@@ -1,19 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class InventoryController : MonoBehaviour
 {
-    public GameObject inventoryPanel; // Reference to your inventory panel
+    public GameObject inventoryPanel;
 
     void Update()
     {
-        // Check if the "I" key is pressed
+        // Check if "i" key is pressed
         if (Input.GetKeyDown(KeyCode.I))
         {
+            // Toggle the inventory panel
+            bool isInventoryOpen = !inventoryPanel.activeSelf;
             // If inventory panel is active, then close it; otherwise, open it
-            inventoryPanel.SetActive(!inventoryPanel.activeSelf);
+            inventoryPanel.SetActive(isInventoryOpen);
+
+            // Show/hide cursor and lock it for inventory
+            if (isInventoryOpen)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
         }
     }
 }
-
